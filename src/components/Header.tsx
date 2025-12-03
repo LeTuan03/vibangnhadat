b88faaddcbe219ba55e30d3046105cc69c75f078
letuan1910 <leetuaans03@gmail.com>
2025-12-03 19:46:46 +0700

    UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaBars, FaTimes, FaPhone, FaChevronDown } from 'react-icons/fa';
 import { navigationItems } from '../data/content';
 import { scrollToElement } from '../utils/helpers';
